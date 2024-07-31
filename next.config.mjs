99558fc12c37bf75374f00d9838b7bb1d3200af8
asfarmunir <asfarmunir12345@gmail.com>
2024-07-31 15:54:21 +0500

    initial push

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    eslint:{
+        //remove these lines if you want to use eslint
+        ignoreDuringBuilds: true,
+    },
+    typescript:{
+        ignoreBuildErrors: true,
+    }
+};
 
 export default nextConfig;
